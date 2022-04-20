e42580b2402edea90590007427130d4c8bf7d8ac
“Akshu864” <bhardwahjakshay14@gmail.com>
2022-04-20 21:30:38 +0530

    assignment-middle-intro

--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const moment=require('moment');
 const bodyParser = require('body-parser');
 const route = require('./routes/route.js');
 const { default: mongoose } = require('mongoose');
@@ -8,13 +9,43 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
 
-mongoose.connect("mongodb+srv://functionup-cohort:G0Loxqc9wFEGyEeJ@cluster0.rzotr.mongodb.net/Pritesh8769811-DB?retryWrites=true&w=majority", {
-    useNewUrlParser: true
+app.use(function(req,res,next){
+    let date = moment().format('YYYY-MM-DD HH:MM:SS')
+    console.log(`${date}, ${req.socket.remoteAddress}, ${req.path}`)
+    next()
 })
-.then( () => console.log("MongoDb is connected"))
-.catch ( err => console.log(err) )
+
 
 app.use('/', route);
+
+
+
+// app.use(function(req, res, next){
+
+//     let timestamp=newDate().getTime()
+//     console.log(timestamp)
+//     next()
+//   });
+
+
+
+
+// function middleware1(req,res,next){
+
+//     let timestamp=moment().format('YYYY-MM-DD HH:mm:ss');
+//     console.log(`${timestamp},${ req.socket.remoteAddress},${req.route.path}`)
+//     next();
+
+    
+// }
+
+
+
+  
+app.get
+
+
+
 
 
 app.listen(process.env.PORT || 3000, function () {
