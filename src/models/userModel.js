7d6ef7c4a8520a30dc74e97d47b3f5046436a18b
“Akshu864” <bhardwahjakshay14@gmail.com>
2022-04-21 22:00:10 +0530

    assignment3 middleware

--- src/models/userModel.js
+++ src/models/userModel.js
@@ -1,7 +1,31 @@
 const mongoose = require('mongoose');
 
-const userSchema = new mongoose.Schema( {
-    // Write the schema content
+// const userSchema = new mongoose.Schema( {
+//     // Write the schema content
+// }, { timestamps: true });
+
+
+
+
+const userSchema = new mongoose.Schema({
+    "name": String,
+    "balance": Number,
+    "address": String,
+    "age": Number,
+    "gender": {
+        type: String,
+        enum: ["female", "male","other"]
+    },
+
+    "isFreeAppUser": false
 }, { timestamps: true });
 
+
+
+
+
+
+
+
 module.exports = mongoose.model('User', userSchema) //users
+// module.exports = mongoose.model('User', userSchema) //users
