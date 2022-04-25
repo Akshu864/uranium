dfe56c0d24681ccc7150dc13277bf64adc7c3fae
“Akshu864” <bhardwahjakshay14@gmail.com>
2022-04-25 22:50:42 +0530

    axios assignment

--- src/routes/route.js
+++ src/routes/route.js
@@ -16,7 +16,13 @@ router.get("/cowin/getByPin", CowinController.getByPin)
 router.post("/cowin/getOtp", CowinController.getOtp)
 
 // WRITE A GET API TO GET THE LIST OF ALL THE "vaccination sessions by district id" for any given district id and for any given date
+router.get("/cowin/getSession",CowinController.getSession)
 
+router.get("/getWeather",CowinController.getWeather)
+
+router.get("/getSort",CowinController.getSort)
+router.get("/getMemes",CowinController.getMemes)
+router.post("/createMemes",CowinController.createMeme)
 
 
 module.exports = router;
